4ef1744276bd481bb349edf28155115bb54c19dc
Christian Kreibich <christian@corelight.com>
2022-01-07 17:56:34 -0800

    Additional packaging tweaks

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 import setuptools.command.build_py
 
 from setuptools import setup
+from setuptools.dist import Distribution
+
+
+class BinaryDistribution(Distribution):
+    """Distribution which always forces a binary package with platform name"""
+    # https://stackoverflow.com/a/62668026
+    def has_ext_modules(_):
+        return True
 
 class BuildCommand(setuptools.command.build_py.build_py):
     """A customized build command that also rebuilds the parser bindings as needed."""
@@ -15,24 +23,31 @@ class BuildCommand(setuptools.command.build_py.build_py):
             print('Warning: tree_sitter module not found, not refreshing bindings')
             return
 
-        # Refresh the tree-sitter binding. No-op when the parser needs no
-        # rebuild. We specify the library output path and the tree-sitter
-        # repo path to include.
+        # Recompile the tree-sitter bindings. This is a no-op when the parser
+        # needs no rebuild. We specify the library output path and the
+        # tree-sitter repo path to include.
         tree_sitter.Language.build_library('./zeekscript/zeek-language.so',
                                            ['tree-sitter-zeek'])
+
 
 setup(
     name='zeekscript',
     version='0.1.1',
     description='A Zeek script formatter and analyzer',
-        maintainer='The Zeek Project',
+    maintainer='The Zeek Project',
     maintainer_email='info@zeek.org',
     url='https://github.com/ckreibich/zeek-script',
+
     scripts=['zeek-script'],
     packages=['zeekscript'],
     package_data={'zeekscript': ['zeek-language.so']},
+
     cmdclass={ 'build_py': BuildCommand },
+    distclass=BinaryDistribution,
+
+    setup_requires=['tree_sitter'],
     install_requires=['tree_sitter'],
+
     classifiers=[
         'Programming Language :: Python :: 3',
         'Topic :: Utilities',
