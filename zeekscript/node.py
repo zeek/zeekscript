e4b9bd505e689eefd1189ad0b0ebfe5e29e7015e
Christian Kreibich <christian@corelight.com>
2022-01-13 15:36:05 -0800

    Track AST vs CST node state explicitly in zeekscript.Node
    This distinguishes abstract and concrete syntax tree nodes and provides separate
    navigation for the two. The existing children/parent members allow navigating
    purely the AST.
    
    Each AST node tracks its "surrounding" CST nodes via a sequence of CST nodes
    preceeding and succeeding it. These CST nodes can be comments or newlines.

--- zeekscript/node.py
+++ zeekscript/node.py
@@ -1,23 +1,57 @@
 class Node:
-    """A subset of tree_sitter.Node.
+    """A relative of tree_sitter.Node.
 
-    We use this to build a mutable clone of the tree-sitter parse tree,
-    and add some convenience functions.
+    This class represents nodes in the syntax tree. A subset of its members
+    match those of tree_sitter.Node. Instances provide separate navigational
+    data structures for abstract and concrete nodes (such as comments, newlines)
+    in the source, and a few helper methods.
     """
     def __init__(self):
+        # AST navigation: these relationship omit nodes present only in the
+        # concrete tree, such as comments and whitespace. The following members
+        # are all present in the TS node type.
         self.children = []
         self.parent = None
         self.prev_sibling = None
         self.next_sibling = None
+
         self.start_byte = 0
         self.end_byte = 0
         self.start_point = (0, 0)
         self.end_point = (0, 0)
         self.is_named = False
+        self.is_missing = False
+        self.has_error = False
         self.type = None
 
+        # Additions over the TS node members below:
+
+        self.prev_cst_sibling = None
+        self.next_cst_sibling = None
+
+        # If this is an AST node: full sequences of CST nodes
+        # preceeding/succeeding this node.
+        self.prev_cst_siblings = []
+        self.next_cst_siblings = []
+
+        # Whether this is an AST member
+        self.is_ast = False
+
+        # For CST nodes, a link to the AST node they're grouped with.
+        self.ast_parent = None
+
+    def is_nl(self):
+        """Returns True iff this is a newline."""
+        return self.is_named and self.type and self.type == 'nl'
+
     def is_comment(self):
-        return self.type and self.type.endswith('_comment')
+        """Returns True iff this is any kind of comment."""
+        return self.is_named and self.type and self.type.endswith('_comment')
+
+    def is_minor_comment(self):
+        """Returns True iff this is a minor comment ("# foo")."""
+        return self.is_named and self.type and self.type == 'minor_comment'
 
     def is_zeekygen_prev_comment(self):
-        return self.type and self.type == 'zeekygen_prev_comment'
+        """Returns True iff this is a Zeekygen "##<" comment."""
+        return self.is_named and self.type and self.type == 'zeekygen_prev_comment'
