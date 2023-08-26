0a7e2912b8d16021b14c3601d8feaa238297d3c2
Benjamin Bannier <benjamin.bannier@corelight.com>
2023-08-26 17:37:50 +0200

    Updating CHANGES and VERSION.

--- zeekscript/__init__.py
+++ zeekscript/__init__.py
@@ -1,5 +1,5 @@
 """Wrapper around more low-level tests."""
-__version__ = "1.2.1-53"
+__version__ = "1.2.3"
 __all__ = ["cli", "error", "formatter", "node", "output", "parser", "script"]
 
 from .cli import *
